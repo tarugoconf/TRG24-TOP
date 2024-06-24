600dd641634494275127a773765de3b26dbf2cf0
Oscar Otero <oom@oscarotero.com>
2024-06-24 21:49:28 +0200

    fix basepath

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
 import lightningcss from "lume/plugins/lightningcss.ts";
+import basePath from "lume/plugins/base_path.ts";
 import icons from "https://deno.land/x/lume_icon_plugins@v0.1.0/phosphor.ts";
 
 const site = lume();
@@ -10,6 +11,7 @@ site.copy("fonts");
 site.copy("files");
 site.copy("scripts");
 site.use(lightningcss());
+site.use(basePath());
 site.use(icons({
   name: "icon",
   defaultType: "duotone",
